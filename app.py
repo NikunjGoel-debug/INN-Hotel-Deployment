4eca81359b209758f119484d9bf3bdaf9867eebb
Nikunj Goel <goelnikunj9@gmail.com>
2024-12-17 12:32:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ def main():
     price=st.text_input('Enter the price offered for the room')
     adults=st.selectbox('Select the no adults in booking',[0,1,2,33,4])
     wkd=st.text_input('Enter the weekend nights in the booking')
-    park=(lambda x:1 is x=='Yes'else 0)(st.selectbox('Is parking included in the booking',['Yes','No']))
+    park=(lambda x:1 if x=='Yes' else 0)(st.selectbox('Is parking included in the booking',['Yes','No']))
     month=st.slider('What will be month of arrival',min_value=1,max_value=12,step=1)
     day=st.slider('What will be day of arrival',min_value=1,max_value=31,step=1)
-    wkday_lambda=(lambda x: 0 if x=='Mon' else 1 if x=='Tue' else 2 if x=='Wed'else 3 if x=='Thus'
+    wkday_lambda=(lambda x: 0 if x=='Mon' else 1 if x=='Tue' else 2 if x=='Wed' else 3 if x=='Thus'
                   else 4 if x=='Fri' else 5 if x=='Sat' else 6)
     wkday=st.selectbox('what us the weekday of arrival',['Mon','Tue','Wed','Thus','Fri','Sat','Sun'])
 
